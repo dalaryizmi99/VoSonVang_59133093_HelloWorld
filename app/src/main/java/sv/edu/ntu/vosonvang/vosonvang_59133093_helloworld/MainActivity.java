5c4ce8960d0edad778fdc1dfacc8c84d19e1e94f
Dalaryizmi <vang12383@gmail.com>
2020-04-09 21:20:26 +0700

    Initial commit

---
+++ app/src/main/java/sv/edu/ntu/vosonvang/vosonvang_59133093_helloworld/MainActivity.java
@@ -0,0 +1,14 @@
+package sv.edu.ntu.vosonvang.vosonvang_59133093_helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
